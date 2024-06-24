113ac7db991c29b4433bfc4272f0e5d3ba43f1b2
Mac <mac@MacBook-Pro-Mac.local>
2024-06-25 02:48:57 +0700

    feat: add

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/darulfh/skuy_pay_be/config"
 	"github.com/darulfh/skuy_pay_be/database"
 	"github.com/darulfh/skuy_pay_be/model"
+	"github.com/darulfh/skuy_pay_be/routes"
+	m "github.com/darulfh/skuy_pay_be/usecase/middlewares"
 
 	"github.com/labstack/echo/v4"
 )
@@ -13,6 +15,14 @@ import (
 func main() {
 
 	config.LoadConfig()
+
+	e := echo.New()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, model.MetaData{
+			Message: "Success",
+		})
+	})
 
 	db, err := database.ConnectDB()
 	if err != nil {
@@ -23,20 +33,13 @@ func main() {
 
 	database.Migrate(db)
 
-	e := echo.New()
-
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, model.MetaData{
-			Message: "Success",
-		})
-	})
 	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 	// 	AllowOrigins: []string{"*"},
 	// 	AllowHeaders: []string{"*"},
 	// }))
 
-	// routes.Routes(e, db)
-	// m.LogMiddlewares(e)
+	routes.Routes(e, db)
+	m.LogMiddlewares(e)
 
 	// ====== HTTP ========
 	e.Logger.Fatal(e.Start(":" + config.AppConfig.AppPort))
