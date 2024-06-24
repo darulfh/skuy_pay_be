6df900bfffbc352936380ae711cd4042e88c05e3
Mac <mac@MacBook-Pro-Mac.local>
2024-06-25 02:33:33 +0700

    feat: add

--- main.go
+++ main.go
@@ -1,47 +1,36 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/darulfh/skuy_pay_be/config"
-	"github.com/darulfh/skuy_pay_be/model"
-	echo "github.com/labstack/echo/v4"
+	"github.com/darulfh/skuy_pay_be/database"
+	"github.com/darulfh/skuy_pay_be/routes"
+	m "github.com/darulfh/skuy_pay_be/usecase/middlewares"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 
 	config.LoadConfig()
 
+	db, err := database.ConnectDB()
+	if err != nil {
+		panic(err)
+	}
+
+	// database.Drop(db)
+
+	database.Migrate(db)
+
 	e := echo.New()
-
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, model.MetaData{
-			Message: "Success",
-		})
-	})
-
-	e.Logger.Fatal(e.Start(":" + config.AppConfig.AppPort))
-
-	// config.LoadConfig()
-
-	// db, err := database.ConnectDB()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// // database.Drop(db)
-
-	// database.Migrate(db)
-
-	// e := echo.New()
 	// e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 	// 	AllowOrigins: []string{"*"},
 	// 	AllowHeaders: []string{"*"},
 	// }))
 
-	// routes.Routes(e, db)
-	// m.LogMiddlewares(e)
+	routes.Routes(e, db)
+	m.LogMiddlewares(e)
 
-	// // ====== HTTP ========
-	// e.Logger.Fatal(e.Start(":" + config.AppConfig.AppPort))
+	// ====== HTTP ========
+	e.Logger.Fatal(e.Start(":" + config.AppConfig.AppPort))
 }
