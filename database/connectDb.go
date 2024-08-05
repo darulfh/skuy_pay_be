02f8b09f92bfaf45dd50fb804410f5dda365a1a6
Mac <mac@MacBook-Pro-Mac.local>
2024-08-05 22:35:55 +0700

    feat: add compose yml

--- database/connectDb.go
+++ database/connectDb.go
@@ -11,13 +11,14 @@ import (
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+	// dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s",
 		config.AppConfig.DBHost,
 		config.AppConfig.DBUsername,
 		config.AppConfig.DBPassword,
 		config.AppConfig.DBName,
 		config.AppConfig.DBPort,
-		config.AppConfig.SSLMode,
+		// config.AppConfig.SSLMode,
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
